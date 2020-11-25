64d5f80ebaab10d45a05ba5abdeff0bf474c570b
Mohamed Tawfik <motawfik98@outlook.com>
2020-11-26 00:50:41 +0200

    Used binding and added a start button

--- app/src/main/java/com/motawfik/sensorpassword/MainActivity.kt
+++ app/src/main/java/com/motawfik/sensorpassword/MainActivity.kt
@@ -15,23 +15,22 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.animation.doOnEnd
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.motawfik.sensorpassword.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var sensorManager: SensorManager
     private var accelerometerSensor: Sensor? = null
     private lateinit var sensorListener: SensorListener
-    private lateinit var firstProgressBar: ProgressBar
-    private lateinit var secondProgressBar: ProgressBar
-    private lateinit var thirdProgressbar: ProgressBar
-
-    private lateinit var firstTextView: TextView
-    private lateinit var secondTextView: TextView
-    private lateinit var thirdTextView: TextView
+    private lateinit var binding: ActivityMainBinding
+    private var animator: ObjectAnimator? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
 
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
@@ -39,39 +38,41 @@ class MainActivity : AppCompatActivity() {
         sensorListener = SensorListener()
         if (accelerometerSensor == null) {
             Toast.makeText(this, "No Accelerometer Found", Toast.LENGTH_LONG).show()
-        }
-
-        firstProgressBar = findViewById(R.id.firstProgressBar)
-        secondProgressBar = findViewById(R.id.secondProgressBar)
-        thirdProgressbar = findViewById(R.id.thirdProgressBar)
-
-        firstTextView = findViewById(R.id.firstTextView)
-        secondTextView = findViewById(R.id.secondTextView)
-        thirdTextView = findViewById(R.id.thirdTextView)
-
-        sensorListener.count = 0
-        var animator = getAnimator(firstProgressBar, firstTextView) // animate the first circular progress bar
-        sensorListener.textView = findViewById(R.id.firstDigit)
-
-        animator.doOnEnd { // wait till the first progress bar finish
-            sensorListener.textView = findViewById(R.id.secondDigit)
-            sensorListener.count = 0
-            animator = getAnimator(secondProgressBar, secondTextView) // animate the second circular progress bar
-            animator.doOnEnd { // wait till the second progress bar finish
-                sensorListener.textView = findViewById(R.id.thirdDigit)
+        } else {
+            binding.startBtn.setOnClickListener {
+                resetUI()
                 sensorListener.count = 0
-                animator = getAnimator(thirdProgressbar, thirdTextView) // animate the third progress bar
+                var animator = getAnimator(
+                    binding.firstProgressBar,
+                    binding.firstTextView
+                ) // animate the first circular progress bar
+                sensorListener.textView = binding.firstDigit
+                animator.doOnEnd { // wait till the first progress bar finish
+                    sensorListener.textView = binding.secondDigit
+                    sensorListener.count = 0
+                    animator = getAnimator(
+                        binding.secondProgressBar,
+                        binding.secondTextView
+                    ) // animate the second circular progress bar
+                    animator.doOnEnd { // wait till the second progress bar finish
+                        sensorListener.textView = binding.thirdDigit
+                        sensorListener.count = 0
+                        getAnimator(
+                            binding.thirdProgressBar,
+                            binding.thirdTextView
+                        ) // animate the third progress bar
+                    }
+                }
             }
         }
-
     }
 
     private fun getAnimator(progressBar: ProgressBar, textView: TextView): ObjectAnimator {
-        val animator: ObjectAnimator = ObjectAnimator.ofInt(progressBar, "progress", 100)
-        animator.duration = 2500 // in milliseconds
-        animator.interpolator = LinearInterpolator()
-        animator.start()
-        animator.addUpdateListener { updatedAnimation ->
+        animator = ObjectAnimator.ofInt(progressBar, "progress", 100)
+        animator?.duration = 2500 // in milliseconds
+        animator?.interpolator = LinearInterpolator()
+        animator?.start()
+        animator?.addUpdateListener { updatedAnimation ->
             when (updatedAnimation.animatedValue.toString().toInt()) {
                 in 20..39 -> {
                     textView.text = getString(R.string.two_seconds)
@@ -90,14 +91,31 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-        return animator
+        return animator!!
     }
 
+    private fun resetUI() {
+        animator?.removeAllListeners()
+        animator?.cancel()
+        binding.firstDigit.text = ""
+        binding.secondDigit.text = ""
+        binding.thirdDigit.text = ""
+        binding.firstTextView.text = getString(R.string.remaining_seconds)
+        binding.secondTextView.text = getString(R.string.remaining_seconds)
+        binding.thirdTextView.text = getString(R.string.remaining_seconds)
+        binding.firstProgressBar.progress = 0
+        binding.secondProgressBar.progress = 0
+        binding.thirdProgressBar.progress = 0
+    }
 
     override fun onResume() {
         super.onResume()
         accelerometerSensor?.also { accelerometer ->
-            sensorManager.registerListener(sensorListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+            sensorManager.registerListener(
+                sensorListener,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
         }
     }
 
